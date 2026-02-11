701769dda461b27dc0e6781a9c5c704a58890247
Guillaume Boucher <guillaumeboucher.contact@gmail.com>
2026-02-11 11:48:54 +0100

    removed unnessary elements

--- src/TestPage.vue
+++ src/TestPage.vue
@@ -1,16 +1,4 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { getFilmData } from '@/api/wordAPI'
-import FilmCard from './components/FilmCard.vue'
-
-const films = ref({ results: [] })
-
-onMounted(async () => {
-  const data = await getFilmData()
-  films.value = data
-  console.log(data.results)
-})
-</script>
+<script setup></script>
 
 <template>
   <h1>AAHHH</h1>
@@ -19,14 +7,7 @@ onMounted(async () => {
     documentation
   </p>
   <ul>
-    <FilmCard
-      v-for="film in films.results"
-      :key="film.id"
-      :name="film.name"
-      :img-url="'https://image.tmdb.org/t/p/w780' + film.backdrop_path"
-    >
-      {{ film.name }}
-    </FilmCard>
+    $
   </ul>
 </template>
 

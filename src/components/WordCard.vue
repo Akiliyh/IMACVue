20a029f52c13dba20971aa1ecb25537ebc14b472
Guillaume Boucher <guillaumeboucher.contact@gmail.com>
2026-03-09 15:09:30 +0100

    fix: border transform wordcard

--- src/components/WordCard.vue
+++ src/components/WordCard.vue
@@ -27,7 +27,7 @@ const props = defineProps({
   width: fit-content;
   height: fit-content;
   border: 3px solid transparent;
-  transition: 0.3s ease-out;
+  transition: border 0.3s ease-out;
 
   &:hover,
   &:focus-visible {

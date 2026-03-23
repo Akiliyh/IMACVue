53efb4b9410b69a6a7c69dfde0f9f8b108a484ca
Guillaume Boucher <guillaumeboucher.contact@gmail.com>
2026-03-23 15:37:53 +0100

    feat: added transition to modal

--- src/components/WordDefinition.vue
+++ src/components/WordDefinition.vue
@@ -29,19 +29,52 @@ const toggleModal = async () => {
     <p>{{ word }}</p>
     <i class="pi pi-question" style="font-size: 0.8rem"></i>
   </div>
-  <dialog v-if="isModalOpen" open id="word-def-dialog">
-    {{ definition }}
-    {{ type }}
-    <button @click="toggleModal()">Close</button>
-  </dialog>
+
 
   <!-- we want to teleport to body so it takes fs -->
   <Teleport to="body">
-    <div @click="toggleModal()" v-if="isModalOpen" class="fallback-background"></div>
+    <Transition>
+      <dialog class="dialog" v-if="isModalOpen" open id="word-def-dialog">
+        <i v-if="!definition" class="pi pi-spin pi-spinner" style="font-size: 2rem"></i>
+        <div v-html="definition"></div>
+        <span>{{ type }}</span>
+        <button @click="toggleModal()">Close</button>
+      </dialog>
+    </Transition>
+    <Transition>
+      <div @click="toggleModal()" v-if="isModalOpen" class="fallback-background"></div>
+    </Transition>
+
   </Teleport>
 </template>
 
 <style lang="scss">
+.dialog {
+  z-index: 2;
+  position: fixed;
+  top: 100px;
+  border-radius: 5px;
+  border: 1px solid grey;
+  display: flex;
+  gap: 10px;
+  max-width: 80vw;
+
+  button {
+    cursor: pointer;
+  }
+
+  &.v-enter-active,
+  &.v-leave-active {
+    transition: 0.5s ease;
+  }
+
+  &.v-enter-from,
+  &.v-leave-to {
+    transform: translateY(50px);
+    opacity: 0;
+  }
+}
+
 .definition {
   cursor: pointer;
   border-radius: 10px;
@@ -59,12 +92,22 @@ const toggleModal = async () => {
 
 .fallback-background {
   cursor: pointer;
-  z-index: 0;
+  z-index: 1;
   width: 100vw;
   height: 100vh;
-  position: absolute;
+  position: fixed;
   left: 0;
   top: 0;
   background-color: rgba(0, 0, 0, 0.06);
+
+  &.v-enter-active,
+  &.v-leave-active {
+    transition: opacity 0.5s ease;
+  }
+
+  &.v-enter-from,
+  &.v-leave-to {
+    opacity: 0;
+  }
 }
 </style>
